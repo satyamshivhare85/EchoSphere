5d3026433f97896f884725482c43a9e63e61ae91
Satyam Shivhare <satyamshivhare229@gmail.com>
2026-02-09 17:27:51 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -54,7 +54,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENVIRONMENT === "production",
     });
 
@@ -103,7 +103,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENVIRONMENT === "production",
     });
 
