a11dc9a06cfcc0ef43e576c0db5f883ab7651de8
Satyam Shivhare <satyamshivhare229@gmail.com>
2026-02-09 17:29:44 +0530

    Fix case of sameSite attribute in cookie settings

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -54,7 +54,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "none",
+      sameSite: "None",
       secure: process.env.NODE_ENVIRONMENT === "production",
     });
 
@@ -103,7 +103,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
+      sameSite: "None",
       secure: process.env.NODE_ENVIRONMENT === "production",
     });
 
